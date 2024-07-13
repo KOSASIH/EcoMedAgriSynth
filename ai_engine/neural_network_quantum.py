e787390afed2f39282205270ee80cc36dae66c24
KOSASIH <kosasihg88@gmail.com>
2024-07-13 16:41:29 +0700

    Create neural_network_quantum.py

---
+++ ai_engine/neural_network_quantum.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from qiskit import QuantumCircuit, execute
+
+class QuantumNeuralNetwork:
+    def __init__(self, input_shape, num_classes):
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
+            tf.keras.layers.Dense(32, activation='relu'),
+            tf.keras.layers.Dense(num_classes, activation='softmax')
+        ])
+        self.quantum_circuit = QuantumCircuit(5, 5)
+
+    def train(self, X_train, y_train, epochs=10, batch_size=32):
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2)
+        self.quantum_circuit.barrier()
+        job = execute(self.quantum_circuit, backend='qasm_simulator', shots=1024)
+        result = job.result()
+        self.model.set_weights(result.get_statevector())
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
