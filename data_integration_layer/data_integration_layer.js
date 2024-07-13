286dc6fbf4e92bbe8072e9945e85bb49c38115d9
KOSASIH <kosasihg88@gmail.com>
2024-07-13 16:29:41 +0700

    Create data_integration_layer.js

---
+++ data_integration_layer/data_integration_layer.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DataIntegrationLayer() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('https://api.example.com/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Data Integration Layer</h1>
+      <p>Data: {JSON.stringify(data)}</p>
+    </div>
+  );
+}
+
+export default DataIntegrationLayer;
